76677f942378dc6d6c2ecfe6d2cdb1f15af9aa55
liezelbael <you@example.com>
2022-01-07 12:18:10 +0800

    I'm done on my final exam...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ExamCalc';
+  title = 'Angular Calculator';
+
+ num1:number = 0
+ num2:number = 0
+ num3:number = 0
+
+add(){
+  this.num3=this.num1+this.num2;
+}
+sub(){
+  this.num3=this.num1-this.num2;
+}
+multiply(){
+  this.num3=this.num1*this.num2;
+}
+divide(){
+  this.num3=this.num1/this.num2;
+}
+
 }
